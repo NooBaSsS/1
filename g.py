21b1b0171be054e9bbab1bce34f700fb215006a1
NooBaSsS <90272720+NooBaSsS@users.noreply.github.com>
2023-02-07 19:45:32 +0300

    Add files via upload

---
+++ g.py
@@ -0,0 +1,39 @@
+import random
+
+garden = ["покрышка", "бутылка", "окурок", "железяка", "череп коровы"]
+garden.clear()
+
+order = (
+    ("ирис", "ирис", "ирис", "ирис", "ирис", "ирис", "ирис", "ирис", "ирис", "ирис"),
+    ("роза", "роза", "роза", "роза", "роза", "роза", "роза", "роза", "роза", "роза"),
+    ("пион", "пион", "пион", "пион", "пион", "пион", "пион", "пион", "пион", "пион")
+)
+
+for package in order:
+    for seed in package:
+        garden.append(seed)
+
+weeds = ("борщевик", "крапива", "лопух")
+
+for weed in weeds:
+    for i in range(random.randint(5, 10)):
+        garden.append(weed)
+
+random.shuffle(garden)
+print("сад до прополки:", garden)
+
+
+print(set(garden))
+
+
+
+while len() == 3: 
+    for a in garden:
+        if a in weeds:
+            garden.remove(a)
+        else:
+            pass
+    
+
+
+print("сад до прополки:", garden)
